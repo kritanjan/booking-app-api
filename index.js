4b2992ae146724bcfd13a56dd31b1b41990fe735
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2023-02-08 18:36:05 +0530

    Setting up the get routes and the file structure

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import authRoute from './routes/auth.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+import usersRoute from './routes/users.js'
 
 const app = express()
-
 dotenv.config()
 
+
+// Mongo DB connetions and controlling.
 const connect = async () => {
-try{
-    await mongoose.connect((process.env.MONGO))
-    console.log('Connected to MongoDB')
-}catch(error){
-    throw error
-}}
+    try {
+        await mongoose.connect((process.env.MONGO))
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        throw error
+    }
+}
 
-mongoose.connection.on('disconnected', ()=>{
-    console.log('disconnected')
+mongoose.connection.on('disconnected', () => {
+    console.log('disconnected from MongoDB')
 })
-mongoose.connection.on('connected',()=>{
-    console.log('connected')
+mongoose.connection.on('connected', () => {
+    console.log('connected to MongoDB')
 })
 
-app.listen(8800, ()=>{
+
+
+//MIDDLEWARES
+app.use('/auth', authRoute)
+app.use('/hotels', hotelsRoute)
+app.use('/rooms', roomsRoute)
+app.use('/users',usersRoute)
+
+
+// App Routes
+
+app.get('/', (req, res) => {
+    res.send('Hello, HomePage')
+})
+
+app.listen(8800, () => {
     connect()
     console.log('Sever started')
 })
---
+++ routes/hotels.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('hello, this is hotels')
+})
+
+
+export default router
