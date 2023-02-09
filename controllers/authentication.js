b63349c78efefbb24129a913234f0fc6dbdf1802
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2023-02-09 18:51:33 +0530

    using Bcrypt to hash

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,12 +1,15 @@
 import User from "../models/User.js"
-
+import bcrypt from 'bcrypt'
 
 export const register = async (req, res, next) => {
     try {
+        const saltRounds = 10;
+        const hash = bcrypt.hashSync(req.body.password, saltRounds);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
         await newUser.save()
         res.status(200).json(newUser)
