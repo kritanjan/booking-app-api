d3eaa62c10a0d0d0d23221143605bf9189e08b59
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2023-02-09 16:44:16 +0530

    created other routes in hotels.js

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,6 +3,8 @@ import Hotel from '../models/Hotel.js';
 
 const router = express.Router();
 
+
+// CREATE ROUTE
 router.post('/', async (req,res)=>{
     const newHotel = Hotel(req.body)
 try {
@@ -11,6 +13,47 @@ try {
 } catch (error) {
     res.send(error).status(500)
 }
+})
+
+
+// UPDATE
+router.put('/:id', async (req,res)=>{  
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body},{new: true})
+        res.status(200).send(updatedHotel)
+    } catch (error) {
+        res.send(error).status(500)
+    }
+})
+
+// DELETE ROUTE
+router.delete('/:id', async (req,res)=>{  
+    try {
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).send('Hotel has been deleted')
+    } catch (error) {
+        res.send(error).status(500)
+    }
+})
+
+// READ ROUTE(SPECIFIC)
+router.get('/:id', async (req,res)=>{  
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).send(hotel)
+    } catch (error) {
+        res.send(error).status(500)
+    }
+})
+
+// READ ROUTE (ALL)
+router.get('/', async (req,res) =>{
+    try {
+        const hotel = await Hotel.find();
+        res.json(hotel).status(200)        
+    } catch (error) {
+        res.send(error).status(500)
+    }
 })
 
 
