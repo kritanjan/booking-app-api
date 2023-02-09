52c716421d58ba4b2962c31e3c9ed65cf8254d27
Kritanjan Bhattacharjee <kritanjan101@gmail.com>
2023-02-09 18:20:45 +0530

    creating user from auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,8 @@
 import express from 'express'
+import { register } from '../controllers/authentication.js';
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('hello, this is auth')
-})
-
-router.get('/register',(req,res)=>{
-    res.send('hello, this is auth/register')
-})
+router.post('/register', register)
 
 export default router
